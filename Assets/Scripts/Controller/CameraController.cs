3082af045ffc1605fe85e810ecc93a1eb0f9cfa1
Usanim <167047045+KCngMn@users.noreply.github.com>
2024-05-09 16:06:29 +0900

    ADD

---
+++ Assets/Scripts/Controller/CameraController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target; // 카메라가 따라다닐 대상 (캐릭터)
+
+    void LateUpdate()
+    {
+        if (target != null)
+        {
+            // 카메라의 위치를 대상(캐릭터)의 위치로 업데이트
+            transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
---
+++ Assets/Scripts/Controller/CharacterController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class CharacterController : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer spriteRenderer;
+
+    void Start()
+    {
+        // 캐릭터의 SpriteRenderer 컴포넌트
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    void Update()
+    {
+        // 마우스 위치와 캐릭터의 위치를 비교하여 뒤집기
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        if (mousePosition.x < transform.position.x)
+        {
+            // 마우스가 캐릭터의 왼쪽에 있을 때
+            spriteRenderer.flipX = true;
+        }
+        else
+        {
+            // 마우스가 캐릭터의 오른쪽에 있을 때
+            spriteRenderer.flipX = false;
+        }
+    }
+}
